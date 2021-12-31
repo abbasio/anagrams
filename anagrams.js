0dc7391b4bf5a9aa1bf9c7748a54bb13311f5d11
abbasio <omarabbasi16@gmail.com>
2021-12-31 13:32:54 -0500

    organized script

--- anagrams.js
+++ anagrams.js
@@ -1,3 +1,34 @@
+//---------GET WORD LIST
+const dictionary = async function() {
+    const wordsURL = "https://gist.githubusercontent.com/dlants/d3b25b0f6c0bf8d023f65e86498bf9e6/raw/b310b5aff00f62f5073b3b8d366f5a639aa88ee3/3000-words.txt";
+    const response = await fetch(wordsURL, {mode: 'cors'});
+    return (await response.text()).split('\n');
+}
+
+//---------ANAGRAM ALGORITHM 
+async function onInput(){
+    const originalWord = document.querySelector('#word-search').value;
+    const anagrams = [];
+    const wordBank = await(dictionary());
+   
+    wordBank.forEach((newWord) => {
+        if ((originalWord.split('').sort().join('') === newWord.split('').sort().join('')) && originalWord != newWord) anagrams.push(newWord);
+    })
+    
+    if (anagrams.length === 0) document.getElementById('output').innerHTML = "No anagrams found :("
+    else document.getElementById('output').innerHTML = `Anagram(s) of ${originalWord}: ${anagrams.join(', ')}`
+}
+
+//---------ENTER KEY FUNCTIONALITY
+document.addEventListener("keyup", function(event){
+    const button = document.querySelector('#button');
+    if (event.key === 'Enter') button.click();
+});
+
+
+
+//---------OLD CODE
+
 // let dictionary;
 
 
@@ -29,35 +60,5 @@
 //   document.getElementById('output').innerHTML = JSON.stringify(o, null, 2)
 // }
 
-// document.addEventListener("keyup", function(event){
-//     if (event.key === 'Enter') button.click();
-// });
 
 
-
-const searchedWord = document.querySelector('#word-search');
-
-
-const dictionary = async function() {
-    const wordsURL = "https://gist.githubusercontent.com/dlants/d3b25b0f6c0bf8d023f65e86498bf9e6/raw/b310b5aff00f62f5073b3b8d366f5a639aa88ee3/3000-words.txt";
-    const response = await fetch(wordsURL, {mode: 'cors'});
-    return (await response.text()).split('\n');
-}
-
-async function onInput(){
-    const originalWord = searchedWord.value;
-    const anagrams = [];
-    const wordBank = await(dictionary());
-    wordBank.forEach((newWord) => {
-        if ((originalWord.split('').sort().join('') === newWord.split('').sort().join('')) && originalWord != newWord) anagrams.push(newWord);
-    })
-    
-    console.log(anagrams);
-    if (anagrams.length === 0) document.getElementById('output').innerHTML = "No anagrams found :("
-    else document.getElementById('output').innerHTML = `Anagram(s) of ${originalWord}: ${anagrams.join(', ')}`
-}
-
-document.addEventListener("keyup", function(event){
-    const button = document.querySelector('#button');
-    if (event.key === 'Enter') button.click();
-});
